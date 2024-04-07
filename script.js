41aaafc83970946cfa12c979d63432ecdc44cdcd
Alan Francis <alanfrancisangra@gmail.com>
2024-04-07 19:07:10 -0300

    Update script.js
    atualização api

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 function updateTokenPrices() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  const lastRow = sheet.getLastRow(); const tokensRange = sheet.getRange('B12:B' + lastRow);
-  const tokens = tokensRange.getValues(); const all_prices = get_prices();
+  const lastRow = sheet.getLastRow();
+  const tokensRange = sheet.getRange('B14:B' + lastRow);
+  const tokens = tokensRange.getValues();
   const prices = tokens.map(row => {
-    const token = row[0]; const price = tokenPrice(token, all_prices); return [price];
-  }); sheet.getRange('C12:C' + lastRow).setValues(prices);
+    const token = row[0];
+    const price = tokenPrice(token);
+    return [price];
+  });
+
+  sheet.getRange('C14:C' + lastRow).setValues(prices);
 }
-function tokenPrice(token, all_prices) {
-  const price = all_prices;
-  if (token == "CROW") {return price.crow}
-  if (token == "PAPYRUS") {return price.papyrus * price.crow}
-  if (token == "MORION") {return price.morion * price.crow}
-  if (token == "PROMOTE") {return price.promote * price.crow}
-  if (token == "GEAR") {return price.gear * price.crow}
-  if (token == "TEAR") {return price.tear * price.crow}
-  if (token == "FEATHER") {return price.feather * price.crow}
+function tokenPrice(token) {
+
+  if (token == "CROW") { return get_price(token) }
+  if (token == "PAPYRUS") { return get_price(token) }
+  if (token == "MORION") { return get_price(token) }
+  if (token == "PROMOTE") { return get_price(token) }
+  if (token == "GEAR") { return get_price(token) }
+  if (token == "TEAR") { return get_price(token) }
+  if (token == "FEATHER") { return get_price(token) }
 }
-function get_crow_price() {
-  const response = UrlFetchApp.fetch(`https://api.wemixplay.com/info/v2/price-chart?symbol=CROW&range=1`);
+
+function get_price(coin) {
+  const response = UrlFetchApp.fetch(`https://api.wemixplay.com/info/v2/price-chart?symbol=${coin}&range=1`);
   const data = JSON.parse(response.getContentText()); return Number(data.data.chart.at(-1).p);
-}
-function get_prices() {
-  const response = UrlFetchApp.fetch(`https://napi.wemixplay.com/info/v1/token/dex/crowToken`);
-  const data = JSON.parse(response.getContentText());
-  const closes = {
-    crow: get_crow_price(),
-    promote: data.Data[0].daySummary.close,
-    gear: data.Data[1].daySummary.close,
-    papyrus: data.Data[2].daySummary.close,
-    feather: data.Data[3].daySummary.close,
-    morion: data.Data[4].daySummary.close,
-    tear: data.Data[5].daySummary.close
-  }; return closes;
 }
